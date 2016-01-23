d643a3d739b2e271dd0383ca76cd4168b0ed83a2
iandreariley <iandreariley@gmail.com>
2016-01-23 10:50:03 -0800

    Change upload controller to parse the data into a map instead of saving the file.

--- src/main/java/edu/cpp/cs580/controller/FileUploadController.java
+++ src/main/java/edu/cpp/cs580/controller/FileUploadController.java
@@ -3,6 +3,8 @@ package edu.cpp.cs580.controller;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
+import edu.cpp.cs580.util.CSVMapper;
 
 @Controller
 public class FileUploadController {
@@ -24,11 +28,12 @@ public class FileUploadController {
             @RequestParam("file") MultipartFile file){
         if (!file.isEmpty()) {
             try {
-                byte[] bytes = file.getBytes();
-                BufferedOutputStream stream =
-                        new BufferedOutputStream(new FileOutputStream(new File(name)));
-                stream.write(bytes);
-                stream.close();
+            	List<Map<String, ? extends Object>> records = CSVMapper.mapCSV(file);
+            	
+            	for(Map<String, ? extends Object> record : records)
+            		for(String key : record.keySet())
+            			System.out.println("key: " + key + ", value: " + (String) record.get(key));
+       
                 return "You successfully uploaded " + name + "!";
             } catch (Exception e) {
                 return "You failed to upload " + name + " => " + e.getMessage();
