f855e1d7ef2e0155647670f20e214a006a55cb15
iandreariley <iandreariley@gmail.com>
2016-01-24 10:51:21 -0800

    Complete mapper class with methods handling the presence and absence of a header row, or header input.

--- src/main/java/edu/cpp/cs580/util/CSVMapper.java
+++ src/main/java/edu/cpp/cs580/util/CSVMapper.java
@@ -1,7 +1,9 @@
 package edu.cpp.cs580.util;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -15,11 +17,26 @@ public class CSVMapper {
 	
 	private static final String CHAR_FORMAT = "UTF-8";
 	
-	public static List<Map<String, ? extends Object>> mapCSV(MultipartFile file) throws Exception {
+	public static List<Map<String, String>> mapCSV(MultipartFile file,
+			boolean fileContainsHeader) throws Exception {
+		
+		if(fileContainsHeader)
+			return mapCSV(file);
+		
+		int columnCount = countColumns(file, ',');
+		String[] header = generateHeader(columnCount);
+		
+		return mapCSV(file, header);
+	}	
+	
+	public static List<Map<String, String>> mapCSV(MultipartFile file)
+			throws Exception {
+		
+		List<Map<String, String>> table = new ArrayList<>();
 		InputStream in = file.getInputStream();
 		InputStreamReader reader = new InputStreamReader(in, CHAR_FORMAT);
-		CSVParser parser = new CSVParser(reader, CSVFormat.RFC4180.withHeader("col1", "col2", "col3"));
-		List<Map<String, ? extends Object>> table = new ArrayList<Map<String, ? extends Object>>();
+		CSVFormat format = CSVFormat.RFC4180;
+		CSVParser parser = new CSVParser(reader, format);
 
 		for(CSVRecord tuple : parser)
 			table.add(tuple.toMap());
@@ -29,5 +46,55 @@ public class CSVMapper {
 		return table;
 	}
 	
+	public static List<Map<String, String>> mapCSV(MultipartFile file,
+			String[] header) throws Exception {
+
+		List<Map<String, String>> table = new ArrayList<>();
+		InputStream in = file.getInputStream();
+		InputStreamReader reader = new InputStreamReader(in, CHAR_FORMAT);
+		CSVFormat format = CSVFormat.RFC4180.withHeader(header);
+		CSVParser parser = new CSVParser(reader, format);
+		
+		for(CSVRecord tuple : parser)
+			table.add(tuple.toMap());
+		
+		parser.close();
+		
+		return table;
+	}
 	
+	private static String[] generateHeader(int columns) {
+		String[] header = new String[columns];
+		for(int i = 0; i < columns; i++)
+			header[i] = "c" + (i+1);
+		return header;
+	}
+	
+	private static int countColumns(MultipartFile file, char separator)
+			throws IOException {
+		
+		InputStream fileStream = file.getInputStream();
+		InputStreamReader input = new InputStreamReader(fileStream);
+		BufferedReader reader = new BufferedReader(input);
+		String row = reader.readLine();
+		
+		if(row == null)
+			return 0;
+		
+		// For n columns there are n - 1 separators, so add 1.
+		int cols = countChars(row, separator) + 1;
+			
+		reader.close();
+		input.close();
+		
+		return cols;
+	}
+	
+	private static int countChars(String s, char c) {
+		int count = 0;
+		for(int i = 0; i < s.length(); i++)
+			if(s.charAt(i) == c)
+				++count;
+		return count;				
+	}
 }
