770c8f9127a73e4a768df08932eb7062367af197
tomwang2011 <tong.wang@liferay.com>
2016-01-25 10:41:41 -0800

    SF

--- src/main/resources/static/js/login-control.js
+++ src/main/resources/static/js/login-control.js
@@ -1,33 +1,33 @@
 function addCustomer() {
 	var email = $('#RegisterEmail').val();
 	var password = $('#RegisterPassword').val();
-
 	var exist;
+
 	if (email) {
 		existCheck(function(result) {
 			exist = result;
 		});
 alert("test: " + exist);
 		$.ajax(
-				{
-					type: "POST",
-					url: "/customers",
-					contentType: "application/json",
-					data: JSON.stringify({
-						"email": email,
-						"password": password
-					}),
-					complete: function(xhr) {
-						if (xhr.readyState == 4) {
-							if (xhr.status == 201) {
-								alert("Created");
-							}
-						}
-						else {
-							alert(JSON.stringfy(xhr));
+			{
+				type: "POST",
+				url: "/customers",
+				contentType: "application/json",
+				data: JSON.stringify({
+					"email": email,
+					"password": password
+				}),
+				complete: function(xhr) {
+					if (xhr.readyState == 4) {
+						if (xhr.status == 201) {
+							alert("Created");
 						}
 					}
-				});
+					else {
+						alert(JSON.stringfy(xhr));
+					}
+				}
+			});
 	}
 	else {
 		alert("Please use a valid email");
@@ -36,19 +36,19 @@ alert("test: " + exist);
 
 function existCheck(callback) {
 	$.ajax(
-			{
-				type : "GET",
-				url  : "/customer/checkExist",
-				async: false,
-				data : {
-				},
-				success : function(result) {
-					$('#status').text(result);
-					callback(result);
-					
-				},
-				error: function (jqXHR, exception) {
-					$('#status').text("Failed to get the status");
-				}
-			});
+		{
+			type : "GET",
+			url  : "/customer/checkExist",
+			async: false,
+			data : {
+			},
+			success : function(result) {
+				$('#status').text(result);
+				callback(result);
+
+			},
+			error: function (jqXHR, exception) {
+				$('#status').text("Failed to get the status");
+			}
+		});
 }
