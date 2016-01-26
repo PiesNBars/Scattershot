ce69ee3c38815dbc2f33505c2dfa79a38ec151a2
tomwang2011 <tong.wang@liferay.com>
2016-01-26 12:03:44 -0800

    Repeat password checker

--- src/main/resources/static/js/login-control.js
+++ src/main/resources/static/js/login-control.js
@@ -1,3 +1,31 @@
+function checkPass()
+{
+    //Store the password field objects into variables ...
+    var pass1 = document.getElementById('password');
+    var pass2 = document.getElementById('RepeatPassword');
+    //Store the Confimation Message Object ...
+    var message = document.getElementById('confirmMessage');
+    //Set the colors we will be using ...
+    var goodColor = "#66cc66";
+    var badColor = "#ffffd8";
+    //Compare the values in the password field
+    //and the confirmation field
+    if(pass1.value == pass2.value){
+        //The passwords match.
+        //Set the color to the good color and inform
+        //the user that they have entered the correct password
+        message.style.color = goodColor;
+        message.innerHTML = "Passwords Match!"
+    }else{
+        //The passwords do not match.
+        //Set the color to the bad color and
+        //notify the user.
+        message.style.color = badColor;
+        message.innerHTML = "Passwords Do Not Match!"
+    }
+}
+
+//Not used, for future reference only
 function addCustomer() {
 	var email = $('#RegisterEmail').val();
 	var password = $('#RegisterPassword').val();
@@ -38,7 +66,7 @@ function addCustomer() {
 		alert("Please use a valid email");
 	}
 }
-
+//Not used, for future reference only
 function existCheck(email, callback) {
 	data = { email: email };
 	$.ajax(
