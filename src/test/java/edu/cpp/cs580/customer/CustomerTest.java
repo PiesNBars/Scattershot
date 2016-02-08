a8d4cd96f3a37233452136afdff61798072de6ff
tomwang2011 <tong.wang@liferay.com>
2016-02-08 11:02:35 -0800

    Add junit test for assignment 6

---
+++ src/test/java/edu/cpp/cs580/customer/CustomerTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.cpp.cs580.customer;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author tom
+ */
+public class CustomerTest {
+	@BeforeClass
+	public static void setUpClass() {
+		_customer = new Customer(
+			"email", "password", "firstName", "lastName");
+	}
+	
+	@Test
+	public void testGetFirstName() {
+		Assert.assertEquals("firstName", _customer.getFirstName());
+	}
+	
+	@Test
+	public void testGetLastName() {
+		Assert.assertEquals("lastName", _customer.getLastName());
+	}
+	
+	private static Customer _customer;
+}
