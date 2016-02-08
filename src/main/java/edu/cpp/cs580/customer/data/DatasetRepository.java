40f28e6102541c690db320383e2afdbe34792ba2
tomwang2011 <tong.wang@liferay.com>
2016-02-08 09:14:55 -0800

    Add method to get all Datasets of the customer

--- src/main/java/edu/cpp/cs580/customer/data/DatasetRepository.java
+++ src/main/java/edu/cpp/cs580/customer/data/DatasetRepository.java
@@ -1,7 +1,9 @@
 package edu.cpp.cs580.customer.data;
 
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface DatasetRepository extends MongoRepository<CustomerDataset, String> {
 	public CustomerDataset findByCustomerIdAndName(String customerId, String name);
+	public List<CustomerDataset> findAllByCustomerId(String customerId);
 }
