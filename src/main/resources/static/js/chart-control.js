f48fd17968c17b7789491903c412ffe75b461b3b
iandreariley <iandreariley@gmail.com>
2016-02-17 13:26:47 -0800

    Remove debugger statements from chart.ftl

--- src/main/resources/static/js/chart-control.js
+++ src/main/resources/static/js/chart-control.js
@@ -1,7 +1,5 @@
 function test (data, chartType, xType, yType) {
-	debugger;
 	var obj = JSON.parse(data);
-	debugger;
 	if(chartType == "line") {
 		$.scattershot.lineChart.create(obj, 1020, 550, xType, yType)
 	} else {
